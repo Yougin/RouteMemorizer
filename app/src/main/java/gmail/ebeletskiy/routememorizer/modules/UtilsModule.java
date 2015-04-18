f524466a15aa4c0dd0752941c3a66709fbcd311d
Eugene Beletskiy <ebeletskiy@gmail.com>
2015-04-18 12:39:11 +0200

    LocationRequest creation added;

--- app/src/main/java/gmail/ebeletskiy/routememorizer/modules/UtilsModule.java
+++ app/src/main/java/gmail/ebeletskiy/routememorizer/modules/UtilsModule.java
@@ -1,6 +1,7 @@
 package gmail.ebeletskiy.routememorizer.modules;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import dagger.Module;
 import dagger.Provides;
 import de.greenrobot.event.EventBus;
@@ -14,14 +15,18 @@ import javax.inject.Singleton;
 @Module(library = true, complete = false)
 public class UtilsModule {
 
+  public static final int SMALLEST_DISPLACEMENT_METERS = 100;
+  public static final int UPDATE_INTERVAL = 5 * 1000;
+  public static final int FASTEST_INTERVAL = 2 * 1000;
+
   @Provides @Singleton GoogleApiClient.Builder provideGoogleApiClientBuilder(App app) {
     return new GoogleApiClient.Builder(app);
   }
 
   @Provides @Singleton ILocationProvider provideUserLocationProvider(GoogleApiClient.Builder client,
       EventBus bus, LocationUpdateEvent locationUpdateEvent,
-      ApiClientConnectedEvent apiClientConnectedEvent) {
-    return new UserLocationProvider(client, bus, apiClientConnectedEvent);
+      ApiClientConnectedEvent apiClientConnectedEvent, LocationRequest locationRequest) {
+    return new UserLocationProvider(client, bus, apiClientConnectedEvent, locationRequest);
   }
 
   @Provides @Singleton ApiClientConnectedEvent provideApiClientConnectedEvent() {
@@ -30,5 +35,14 @@ public class UtilsModule {
 
   @Provides @Singleton LocationUpdateEvent provideLocationUpdateEvent() {
     return new LocationUpdateEvent();
+  }
+
+  @Provides @Singleton LocationRequest provideLocationRequest() {
+    LocationRequest locationRequest = LocationRequest.create();
+    locationRequest.setInterval(UPDATE_INTERVAL);
+    locationRequest.setFastestInterval(FASTEST_INTERVAL);
+    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    locationRequest.setSmallestDisplacement(SMALLEST_DISPLACEMENT_METERS);
+    return locationRequest;
   }
 }
