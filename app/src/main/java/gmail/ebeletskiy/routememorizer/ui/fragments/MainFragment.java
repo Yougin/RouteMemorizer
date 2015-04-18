02bb7e2a9f1ea7b7b9555d35a0ef4295a0521d63
Eugene Beletskiy <ebeletskiy@gmail.com>
2015-04-18 14:33:40 +0200

    Main presenter injected in MainFragment and started.

--- app/src/main/java/gmail/ebeletskiy/routememorizer/ui/fragments/MainFragment.java
+++ app/src/main/java/gmail/ebeletskiy/routememorizer/ui/fragments/MainFragment.java
@@ -13,12 +13,15 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 import gmail.ebeletskiy.routememorizer.R;
 import gmail.ebeletskiy.routememorizer.modules.screens.MainModule;
+import gmail.ebeletskiy.routememorizer.modules.screens.MainPresenter;
 import gmail.ebeletskiy.routememorizer.services.LocationService;
 import gmail.ebeletskiy.routememorizer.ui.base.InjectableFragment;
+import javax.inject.Inject;
 import org.jetbrains.annotations.NotNull;
 
 public class MainFragment extends InjectableFragment implements MainView {
 
+  @Inject MainPresenter presenter;
   @InjectView(R.id.main_listview) ListView listView;
 
   @Override public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
@@ -26,6 +29,16 @@ public class MainFragment extends InjectableFragment implements MainView {
     View view = inflater.inflate(R.layout.fragment_main, container, false);
     ButterKnife.inject(this, view);
     return view;
+  }
+
+  @Override public void onResume() {
+    super.onResume();
+    presenter.onResume();
+  }
+
+  @Override public void onPause() {
+    super.onPause();
+    presenter.onPause();
   }
 
   @Override public void setAdapter(@NotNull BaseAdapter adapter) {
